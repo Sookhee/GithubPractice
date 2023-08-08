6595be7c4004f5b142c6225a9ceb2f470f570cda
정민지 <s2018s38@e-mirim.hs.kr>
2023-08-08 19:05:57 +0900

    init project

---
+++ app/src/main/java/com/github/sookhee/githubpractice/MainActivity.kt
@@ -0,0 +1,11 @@
+it package com.github.sookhee.githubpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
